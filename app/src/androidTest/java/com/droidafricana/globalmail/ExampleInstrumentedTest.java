466f3d411f3679c3193a95e7f59b54ef5734d465
Fbada006 <ferdinandkamata@yahoo.com>
2020-01-23 11:18:19 +0300

    -Fixed failing basic context test

--- app/src/androidTest/java/com/droidafricana/globalmail/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/droidafricana/globalmail/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.disruption.globalmail", appContext.getPackageName());
+        assertEquals("com.droidafricana.globalmail", appContext.getPackageName());
     }
 }
